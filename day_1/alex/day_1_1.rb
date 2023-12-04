cd3abe1f8bf99fea7d88c4d407b030211c2e7eb7
Alex Stearn <53116798+Alex-450@users.noreply.github.com>
2023-12-04 17:03:34 +0100

    Simplify regex

--- day_1/alex/day_1_1.rb
+++ day_1/alex/day_1_1.rb
@@ -1,22 +1,8 @@
 puzzle_input = File.read('./puzzle_inputs/day_1/full_input_day_1.txt').lines
 
-answer = 0
-
-puzzle_input.each do |line|
-  number = line.scan(/\d{1}/)
-
-  first = number[0]
-  last = number[-1]
-  result = "#{first}#{last}".to_i
-
-  answer += result
-end
-
-p "Part 1 answer: #{answer}"
-
-answer2 = 0
-
-numbers = {
+PART_1_REGEX = /\d{1}/
+PART_2_REGEX = /(?=(\d{1}|one|two|three|four|five|six|seven|eight|nine))/
+NUMBERS = {
   'one' => 1,
   'two' => 2,
   'three' => 3,
@@ -26,41 +12,51 @@ numbers = {
   'seven' => 7,
   'eight' => 8,
   'nine' => 9
-}
+}.freeze
+
+answer = 0
+
+def first_and_last(line, regex)
+  first_and_last = line.scan(regex).flatten
+  [first_and_last[0], first_and_last[-1]].map { |number| NUMBERS[number] || number }
+end
 
 puzzle_input.each do |line|
-  results = []
-  line.length.times do |i|
-    test = line[i..-1].scan(/(\d{1}|one|two|three|four|five|six|seven|eight|nine)/)
-    results << test
-  end
+  first_and_last = first_and_last(line, PART_1_REGEX)
 
-  results = results.reject! { |r| r.empty? }
+  result = first_and_last.join.to_s.to_i
 
-  first = results.first[0].first
-  last = results.last[-1].first
+  answer += result
+end
 
-  first = numbers[first] if numbers[first]
+print "Part 1 answer: #{answer}"
 
-  last = numbers[last] if numbers[last]
+answer2 = 0
 
-  result = "#{first}#{last}".to_i
-
+puzzle_input.each do |line|
+  first_and_last = first_and_last(line, PART_2_REGEX)
+  result = first_and_last.join.to_s.to_i
   answer2 += result
 end
 
-p "Part 2 answer: #{answer2}"
+print "\n"
+print "Part 2 answer: #{answer2}"
+print "\n"
 
 # tests
 
-if answer != 54644
-  p 'Part 1 test failed ❌'
+if answer != 54_644
+  print 'Part 1 test failed ❌'
 else
-  p 'Part 1 test passed ✅'
+  print 'Part 1 test passed ✅'
 end
 
-if answer2 != 53348
-  p 'Part 2 test failed ❌'
+print "\n"
+
+if answer2 != 53_348
+  print 'Part 2 test failed ❌'
 else
-  p 'Part 2 test passed ✅'
+  print 'Part 2 test passed ✅'
 end
+
+print "\n"
