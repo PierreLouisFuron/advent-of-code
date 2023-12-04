10a0191807bc3fcbfba5b8d557a923826f7a29c5
Alex Stearn <53116798+Alex-450@users.noreply.github.com>
2023-12-04 15:55:25 +0100

    Day 2 part 2

--- day_2/alex/day_2.rb
+++ day_2/alex/day_2.rb
@@ -10,12 +10,16 @@ BLUE_CUBES_NO = 14
 
 possible_game_ids = []
 
-puzzle_input.each do |line|
-  game_id = line.match(/\d+/)[0]
-
+def set_max_game_counts(line)
   %w[green red blue].each do |colour|
     instance_variable_set("@#{colour}_counts", line.scan(/(\d+) #{colour}/).flatten.map(&:to_i))
   end
+end
+
+puzzle_input.each do |line|
+  game_id = line.match(/\d+/)[0]
+
+  set_max_game_counts(line)
 
   next if @blue_counts.max() > BLUE_CUBES_NO || @green_counts.max() > GREEN_CUBES_NO || @red_counts.max() > RED_CUBES_NO
 
@@ -24,3 +28,24 @@ end
 
 # answer = 2331
 p possible_game_ids.sum
+
+
+# Challenge 2
+# The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively.
+# Adding up these five powers produces the sum 2286.
+# For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+
+game_powers = []
+
+puzzle_input.each do |line|
+  game_id = line.match(/\d+/)[0]
+
+  set_max_game_counts(line)
+
+  game_power = @green_counts.max() * @blue_counts.max() * @red_counts.max()
+
+  game_powers << game_power
+end
+
+# answer = 71585
+p game_powers.sum
