0066cb11cc340b32775b37076fd398398212bc5c
Alex Stearn <53116798+Alex-450@users.noreply.github.com>
2023-12-06 22:09:45 +0100

    Fix up prep_day

--- prep_day.rb
+++ prep_day.rb
@@ -14,12 +14,12 @@ Dir.mkdir("#{dir_name}/#{user}") unless File.exist?("#{dir_name}/#{user}")
 
 # create rb file
 starting_string = <<-BOILERPLATE
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  puzzle_input = File.read('./puzzle_inputs/day_#{day}/full_input_day_#{day}.txt').lines
-    puzzle_input.each do |line|
-      p line
-    end
+puzzle_input = File.readlines('./puzzle_inputs/day_#{day}/full_input_day_#{day}.txt', chomp: true)
+puzzle_input.each do |line|
+  p line
+end
 BOILERPLATE
 
 File.open("#{dir_name}/#{user}/day_#{day}.rb", 'w') { |f| f.write(starting_string) }
