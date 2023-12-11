5725b3af3536192ef756cb958656730311b911f0
Alex Stearn <53116798+Alex-450@users.noreply.github.com>
2023-12-11 13:40:03 +0100

    Day 6 part 2

--- day_6/alex/day_6.rb
+++ day_6/alex/day_6.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 puzzle_input = File.readlines('./puzzle_inputs/day_6/full_input_day_6.txt', chomp: true)
-sample_input = File.readlines('./puzzle_inputs/day_6/sample_input_day_6.txt', chomp: true)
+# sample_input = File.readlines('./puzzle_inputs/day_6/sample_input_day_6.txt', chomp: true)
 
-def winning_possibilites(time, record)
+def get_record_beating_count(time, record)
   record_beating_options = 0
   (0..time).each do |second|
     distance = second * (time - second)
@@ -12,13 +12,17 @@ def winning_possibilites(time, record)
   record_beating_options
 end
 
+def one_race_numbers(input)
+  [input.first.scan(/\d+/).join, input.last.scan(/\d+/).join]
+end
+
 def calculate_winning_options(input)
   record_beating_options = []
   input.first.scan(/\d+/).length.times do |race|
     time = input.first.scan(/\d+/)[race].to_i
     record = input.last.scan(/\d+/)[race].to_i
-    winning_option_count = winning_possibilites(time, record)
-    record_beating_options << winning_option_count
+    record_beating_option_count = get_record_beating_count(time, record)
+    record_beating_options << record_beating_option_count
   end
   record_beating_options.inject(:*)
 end
@@ -27,4 +31,14 @@ winning_amount = calculate_winning_options(puzzle_input)
 
 # Answer part 1: 781200
 print winning_amount
+print "\n"
+
+# For part 2 the numbers should be joined together:
+# There's really only one race - ignore the spaces between the numbers on each line.
+input = one_race_numbers(puzzle_input)
+
+winning_amount_p2 = calculate_winning_options(input)
+
+# Answer part 2: 49240091
+print winning_amount_p2
 print "\n"
