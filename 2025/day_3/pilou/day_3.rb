87d3b725e6160b727222937f9e688dcaa635c530
Pierre-Louis Furon <pierre-louis.furon@fuga.com>
2025-12-03 13:50:13 +0100

    day 3 - part 2

--- 2025/day_3/pilou/day_3.rb
+++ 2025/day_3/pilou/day_3.rb
@@ -15,16 +15,30 @@ def get_max_and_index(bank)
   [max_b, index]
 end
 
-def get_highest_joltage(bank)
-  bank = bank.chars.map { |e| e.to_i }
-  first_battery, first_battery_index = get_max_and_index(bank[0..(bank.length - 2)])
-  second_battery, second_battery_index = get_max_and_index(bank[(first_battery_index + 1)..])
-  "#{first_battery}#{second_battery}".to_i
+# PART 1
+# def get_highest_joltage(bank, joltage_length)
+#   bank = bank.chars.map(&:to_i)
+#   first_battery, first_battery_index = get_max_and_index(bank[0..(bank.length - 2)])
+#   second_battery, = get_max_and_index(bank[(first_battery_index + 1)..])
+#   "#{first_battery}#{second_battery}".to_i
+# end
+
+# PART 2 - that also works with part 1 if joltage_length == 2
+def get_highest_joltage(bank, joltage_length)
+  bank = bank.chars.map(&:to_i)
+  batteries = []
+  current_index = 0
+  (0..(joltage_length - 1)).each do |j_index|
+    battery, battery_index = get_max_and_index(bank[current_index..bank.length - joltage_length + j_index])
+    batteries << battery
+    current_index += battery_index + 1
+  end
+  batteries.join.to_i
 end
 
 total = 0
 puzzle_input.each do |bank|
-  total += get_highest_joltage(bank)
+  total += get_highest_joltage(bank, 12)
 end
 
 p "total = #{total}"
