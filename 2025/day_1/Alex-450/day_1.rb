9fb63777171cdb5f80330d4772f9cadbeb2dce26
Alex Stearn <alex.wmstearn@gmail.com>
2025-12-05 15:12:21 +0100

    day 1 pt.1 alex

--- 2025/day_1/Alex-450/day_1.rb
+++ 2025/day_1/Alex-450/day_1.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-# puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1_test.txt', chomp: true)
-puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1.txt', chomp: true)
+puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1_test.txt', chomp: true)
+# puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1.txt', chomp: true)
 
-position = 50
+@position = 50
 @count = 0
 
 puzzle_input.each do |line|
   direction = line.split('').first
   number_of_turns = line.scan(/\d/).join.to_i % 100
 
-  if direction == 'L'
-    position -= number_of_turns
-  else
-    position += number_of_turns
+  number_of_turns = -number_of_turns if direction == 'L'
+
+  @position += number_of_turns
+
+  if @position.negative?
+    @position = 100 + @position
   end
 
-  if position.negative?
-    position = 100 + position
+  if @position >= 100
+    @position = @position - 100
   end
 
-  if position >= 100
-    position = position - 100
-  end
-
-  if position.zero?
+  if @position.zero?
     @count += 1
   end
 end
