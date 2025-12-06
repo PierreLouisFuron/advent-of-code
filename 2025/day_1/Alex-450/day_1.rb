e3ebe9b7b1f5f8eac7cce3e0f5ec822a61fd2f82
Alex Stearn <alex.wmstearn@gmail.com>
2025-12-06 19:04:37 +0100

    day 1 pt.2 alex

--- 2025/day_1/Alex-450/day_1.rb
+++ 2025/day_1/Alex-450/day_1.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1_test.txt', chomp: true)
-# puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1.txt', chomp: true)
+# puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1_test.txt', chomp: true)
+puzzle_input = File.readlines('./2025/puzzle_inputs/day_1/full_input_day_1.txt', chomp: true)
 
-@position = 50
-@count = 0
+
+# part 1
+position = 50
+count = 0
 
 puzzle_input.each do |line|
   direction = line.split('').first
@@ -12,19 +14,27 @@ puzzle_input.each do |line|
 
   number_of_turns = -number_of_turns if direction == 'L'
 
-  @position += number_of_turns
+  position += number_of_turns
+  
+  count += 1 if (position % 100).zero?
+end
 
-  if @position.negative?
-    @position = 100 + @position
-  end
+puts "part 1: #{count}"
 
-  if @position >= 100
-    @position = @position - 100
-  end
+# part 2
+position = 50
+count = 0
 
-  if @position.zero?
-    @count += 1
+puzzle_input.each do |line|
+  direction = line.split('').first
+  number_of_turns = line.scan(/\d/).join.to_i
+
+  number_of_turns.times do
+    position += 1 if direction == 'L'
+    position -= 1 if direction == 'R'
+
+    count += 1 if position % 100 == 0
   end
 end
 
-puts "Count is: #{@count}"
+puts "part 2: #{count}"
