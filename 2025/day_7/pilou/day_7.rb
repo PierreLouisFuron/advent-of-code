38fe1e79d6215e095f5f8104d533a106c53d8857
Pierre-Louis Furon <pierre-louis.furon@fuga.com>
2025-12-09 12:36:55 +0100

    day 7 - part 2

--- 2025/day_7/pilou/day_7.rb
+++ 2025/day_7/pilou/day_7.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-puzzle_input = File.readlines('./2025/puzzle_inputs/day_7/full_input_day_7_test.txt', chomp: true)
-# puzzle_input = File.readlines('./2025/puzzle_inputs/day_7/full_input_day_7.txt', chomp: true)
+# puzzle_input = File.readlines('./2025/puzzle_inputs/day_7/full_input_day_7_test.txt', chomp: true)
+puzzle_input = File.readlines('./2025/puzzle_inputs/day_7/full_input_day_7.txt', chomp: true)
 
 def get_starting_index(line)
   line.chars.each_with_index do |char, index|
@@ -9,33 +9,45 @@ def get_starting_index(line)
   end
 end
 
+def increase_beam(hash, index, value)
+  hash[index] = hash.keys.include?(index) ? (hash[index] + value) : value
+  hash
+end
+
 def travel_light(line, beam_indexes)
   return [0, beam_indexes] if line.chars.uniq == ['.']
 
   split_beams_total = 0
-  new_beam_indexes = []
-  beam_indexes.each do |beam_index|
+  new_beam_indexes = {}
+  beam_indexes.keys.each do |beam_index|
     if line[beam_index] == '^'
       split_beams_total += 1
-      new_beam_indexes << [beam_index - 1, beam_index + 1]
+      new_beam_indexes = increase_beam(new_beam_indexes, beam_index - 1, beam_indexes[beam_index])
+      new_beam_indexes = increase_beam(new_beam_indexes, beam_index + 1, beam_indexes[beam_index])
     else
-      new_beam_indexes << beam_index
+      new_beam_indexes = increase_beam(new_beam_indexes, beam_index, beam_indexes[beam_index])
     end
   end
-  new_beam_indexes = new_beam_indexes.flatten.uniq
   [split_beams_total, new_beam_indexes]
 end
 
-beam_indexes = []
+beam_indexes = {}
 
 total = 0
 puzzle_input.each_with_index do |line, index|
-  if index == 0
-    beam_indexes << get_starting_index(line)
+  if index.zero?
+    beam_indexes[get_starting_index(line)] = 1
   else
     split_beams_total, beam_indexes = travel_light(line, beam_indexes)
     total += split_beams_total
   end
+end
+
+p "total : #{total}"
+
+total = 0
+beam_indexes.each_value do |value|
+  total += value
 end
 
 p "total : #{total}"
