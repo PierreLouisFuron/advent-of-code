beb3db0876e180cb77fb5f528bdca0d3c11f5348
Pierre-Louis Furon <pierre-louis.furon@fuga.com>
2025-12-10 14:38:30 +0100

    add test parameter for templates

--- prep_day.rb
+++ prep_day.rb
@@ -16,15 +16,17 @@ Dir.mkdir("#{dir_name}/#{USERNAME}") unless File.exist?("#{dir_name}/#{USERNAME}
 starting_string = <<~BOILERPLATE
   # frozen_string_literal: true
 
-  puzzle_input = File.readlines('./#{CURRENT_YEAR}/puzzle_inputs/day_#{day}/full_input_day_#{day}_test.txt', chomp: true)
-  # puzzle_input = File.readlines('./#{CURRENT_YEAR}/puzzle_inputs/day_#{day}/full_input_day_#{day}.txt', chomp: true)
+  test_parameter = ARGV.first
+  is_test_mode_enabled = ['-t', '--test'].include?(test_parameter)
+  filepath = "./#{CURRENT_YEAR}/puzzle_inputs/day_#{day}/full_input_day_#{day}\#{'_test' if is_test_mode_enabled}.txt"
+  puzzle_input = File.readlines(filepath, chomp: true)
 
   puzzle_input.each do |line|
     p line
   end
 BOILERPLATE
 
-File.open("#{dir_name}/#{USERNAME}/day_#{day}.rb", 'w') { |f| f.write(starting_string) }
+File.write("#{dir_name}/#{USERNAME}/day_#{day}.rb", starting_string)
 
 # download puzzle description
 scrape_puzzle_description(day, "#{dir_name}/puzzle.md")
